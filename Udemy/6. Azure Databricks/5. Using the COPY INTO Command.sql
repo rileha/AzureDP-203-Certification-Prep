84e4bd94a3d0d15ffdfd6bfaa6ee590209f8d10c
rileha <hugoriley72@gmail.com>
2024-07-17 20:28:00 +0100

    Update 5. Using the COPY INTO Command.sql

--- Udemy/6. Azure Databricks/5. Using the COPY INTO Command.sql
+++ Udemy/6. Azure Databricks/5. Using the COPY INTO Command.sql
@@ -7,7 +7,7 @@ CREATE TABLE logdata
 %scala
 spark.conf.set(
     "fs.azure.account.key.datalakez95pevs1.dfs.core.windows.net",
-    "rtzbGEf8o/0vWN2myNq8WhgmMdVodR6bjrpKdD4Vco1l4UITjrDJm03/mwRo3wh8unOo1mLoy3ch+AStxuCbOQ==")
+    "KEY")
 
 COPY INTO logdata
 FROM 'abfss://parquet@datalakez95pevs1.dfs.core.windows.net/ActivityLog01 (1).parquet'
